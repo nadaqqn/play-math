bbbbbd6e44d861d28b3bbadcb96b26b10b8ca4fb
nadaqqn <qqotrunnada22@gmail.com>
2024-06-28 18:27:34 +0700

    commit playmath

---
+++ y/js/database-connection.js
@@ -0,0 +1,17 @@
+    import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+  
+  
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyCrbhzDQpFgvIn72T4-4WFYLVpZbQTbE5Y",
+    authDomain: "playmath-2b56a.firebaseapp.com",
+    databaseURL: "https://playmath-2b56a-default-rtdb.firebaseio.com",
+    projectId: "playmath-2b56a",
+    storageBucket: "playmath-2b56a.appspot.com",
+    messagingSenderId: "104465633614",
+    appId: "1:104465633614:web:b2a1c97e9fdce562a80e1f"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
